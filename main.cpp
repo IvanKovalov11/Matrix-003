9e60380e30cb46a9f8d5a7b4e8e6f7b88afecba6
IvanKovalov11 <32193366+IvanKovalov11@users.noreply.github.com>
2018-01-11 13:51:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -226,31 +226,38 @@ bool read_matrix(float ** & elements, unsigned int & result_rows, unsigned int &
 	char symbol;
 	ifstream stream;
 	stream.open(s);
-	if ((stream >> result_rows) && (stream >> symbol) && (stream >> result_columns)) 
-	{
-		create(elements, result_rows, result_columns);
-		bool success = true;
-		for (int i = 0; i < result_rows && success; ++i)
-		{
-			for (int j = 0; j < result_columns; ++j) 
+	if (!(stream.is_open())
+	    {
+		    cout << "File cannot be open";
+	    }
+	    else
+	    {
+ 	     	    if ((stream >> result_rows) && (stream >> symbol) && (stream >> result_columns)) 
+		    {
+			create(elements, result_rows, result_columns);
+			bool success = true;
+			for (int i = 0; i < result_rows && success; ++i)
 			{
-				if (!(stream >> elements[i][j])) 
+				for (int j = 0; j < result_columns; ++j) 
 				{
-					success = false;
-					break;
+					if (!(stream >> elements[i][j])) 
+					{
+						success = false;
+						break;
+					}
 				}
 			}
+			stream.close();
+			return success;
+
 		}
-		stream.close();
-		return success;
 
-	}
-
-	else 
-	{
-		stream.close();
-		return false;
-	}
+		else 
+		{
+			stream.close();
+			return false;
+		}
+	    }
 }
 
 void destroy(float ** elements, unsigned int rows, unsigned int columns) 
